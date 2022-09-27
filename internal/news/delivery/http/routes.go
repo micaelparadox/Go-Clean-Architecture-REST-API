2d8c764f347257365173d5dea80e614991659225
Micael Santana <micaelparadox@gmail.com>
2022-09-27 09:32:31 -0300

    first commit

---
+++ internal/news/delivery/http/routes.go
@@ -0,0 +1,18 @@
+package http
+
+import (
+	"github.com/labstack/echo/v4"
+
+	"github.com/micaelparadox/api-mc/internal/middleware"
+	"github.com/micaelparadox/api-mc/internal/news"
+)
+
+// Map news routes
+func MapNewsRoutes(newsGroup *echo.Group, h news.Handlers, mw *middleware.MiddlewareManager) {
+	newsGroup.POST("/create", h.Create(), mw.AuthSessionMiddleware, mw.CSRF)
+	newsGroup.PUT("/:news_id", h.Update(), mw.AuthSessionMiddleware, mw.CSRF)
+	newsGroup.DELETE("/:news_id", h.Delete(), mw.AuthSessionMiddleware, mw.CSRF)
+	newsGroup.GET("/:news_id", h.GetByID())
+	newsGroup.GET("/search", h.SearchByTitle())
+	newsGroup.GET("", h.GetNews())
+}
